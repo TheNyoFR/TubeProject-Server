11f1f886702c1f381a005bdf0c38e73a0079461c
Retrovers <lucas13430@gmail.com>
2018-01-28 21:02:49 +0100

    Premiere implementation du serveur ftp

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+//REQUIRE
+var ftp = require("ftp-srv")
+
+
+console.log('-- ProjectTube Server --')
+
+//FTP SERVER INIT
+var ftpServ = new ftp("ftp://127.0.0.1:250", {});
+ftpServ.on('login', ({connection, username, password}, resolve, reject) => {
+    if (username != "root" && password != "root"){reject("auth")}
+    resolve({root : __dirname + "/Video/Temp/"})
+}) 
+
+ftpServ.listen().then(() => {
+    console.log("FTP server is running !")
+});
